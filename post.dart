fdedf2e58d535199a865069be211eb92e3d410ad
imv26 <imv26@drexel.edu>
2022-03-03 16:53:25 +0000

    Replace post.dart

--- post.dart
+++ post.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:drexeltwo/tab.dart';
 
 class Post extends StatelessWidget {
   const Post({Key? key}) : super(key: key);
@@ -27,26 +26,89 @@ class MyStatelessWidget extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             const ListTile(
-              //title: Text('The Enchanted Nightingale'),
-              subtitle: Text('Club meeting tonight.'),
+              title: Text('Club meeting tonight.'),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: <Widget>[
                 TextButton(
                   child: const Text('comment'),
-                  onPressed: () {/* ... */},
+                  onPressed: () {
+                    MyStatefulWidget;
+                  },
                 ),
-                const SizedBox(width: 8),
+                const SizedBox(width: 20),
                 TextButton(
                   child: const Icon(Icons.favorite),
-                  onPressed: () {/* ... */},
+                  onPressed: () {},
                 ),
                 const SizedBox(width: 8),
               ],
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Visibility(
+                  visible: true,
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                        hintText: 'Enter your comment',
+                        //border: OutlineInputBorder(),
+                        constraints:
+                            BoxConstraints(maxWidth: 1000, minWidth: 100)),
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<StatefulWidget> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Enter your comment',
+            ),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Validate will return true if the form is valid, or false if
+                // the form is invalid.
+                if (_formKey.currentState!.validate()) {
+                  // Process data.
+                }
+              },
+              child: const Text('comment'),
+            ),
+          ),
+        ],
       ),
     );
   }
